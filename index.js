ff792b4a422666bc7bac2d28573d9b5e7070fb13
nico.nardi@ <plantacaotica@plantas-MacBook-Pro.local>
2023-11-07 19:27:15 +0100

    changes

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ function increment(counterId) {
   let count = parseInt(countEl.textContent) || 0;
   count += 1;
   countEl.textContent = count;
+  updateSumSquare();
 }
 
 function save(counterId) {
@@ -13,4 +14,21 @@ function save(counterId) {
   let countStr = count + " - ";
   saveEl.textContent += countStr;
   countEl.textContent = 0;
+  updateSumSquare();
+}
+function updateSumSquare() {
+  let count1 = parseInt(document.getElementById("count-el1").textContent) || 0;
+  let count2 = parseInt(document.getElementById("count-el2").textContent) || 0;
+  let count3 = parseInt(document.getElementById("count-el3").textContent) || 0;
+
+  let sum = count1 - count2 - count3;
+  let sumSquareContent = document.getElementById("sum-square-content");
+
+  if (sum > 0) {
+    sumSquareContent.textContent = "TE AMO AMORE KEEP BEEN POSITIVE";
+  } else if (sum < 0) {
+    sumSquareContent.textContent = "AMORE STOP COMPLAINING AND HURTING YOURSELF PLEASE. LOVE ME MORE.";
+  } else {
+    sumSquareContent.textContent = "ITS ALL BALANCED. PLEASE TELL ME TE AMO MORE";
+  }
 }
